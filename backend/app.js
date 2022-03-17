c6be5e75d22689eb7f6de1c163b0ae03166ef947
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-03-17 20:26:17 +0300

    fix: исправить настройки cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'mesto22.nomoredomains.work',
+  origin: ['https://mesto22.nomoredomains.work', 'http://mesto22.nomoredomains.work'],
   credentials: true,
 }));
 
